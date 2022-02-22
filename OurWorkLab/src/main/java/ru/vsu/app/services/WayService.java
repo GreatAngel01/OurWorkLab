816b081be2753eb839dc0a2ac6a3116c3c00d160
Stanislav <90761337+GreatAngel01@users.noreply.github.com>
2022-02-22 15:02:04 +0300

    0.1.5 + WayGen

---
+++ OurWorkLab/src/main/java/ru/vsu/app/services/WayService.java
@@ -0,0 +1,10 @@
+package ru.vsu.app.services;
+
+import ru.vsu.app.models.GameMap;
+import ru.vsu.app.models.Node;
+
+public class WayService {
+    public static int[] generateWay(Node start, Node end, GameMap map) {
+        return null;
+    }
+}
